e469eb514c2021cbc96923b644fc09923ac8d1fb
Adrien Caillard <adrien.caillard@gmail.com>
2017-10-19 07:15:14 +0000

    adding plugin files

---
+++ core/class/genepi.class.php
@@ -0,0 +1,177 @@
+<?php
+
+/* This file is part of Jeedom.
+ *
+ * Jeedom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Jeedom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* * ***************************Includes********************************* */
+require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
+
+class genepi extends eqLogic {
+    /*     * *************************Attributs****************************** */
+
+
+
+    /*     * ***********************Methode static*************************** */
+
+    // send a jsonRPC request to Daemon
+    public static function sendToDaemon($method, $param = null) {
+
+        $daemonURL = "http://127.0.0.1:8081/";
+
+        log::add('genepi','debug','RPC call with method: ' . $method);
+
+        $json = json_encode([
+            "jsonrpc" => "2.0",
+            "id" => 1,
+            "method" => $method,
+            "param" => $param,
+        ]);
+
+        $curl = curl_init($daemonURL);
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 5);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',      
+        ));
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $json);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        $curl_response = curl_exec($curl);
+        curl_close($curl);
+
+        if(curl_errno($curl)){
+            log::add('genepi','error','Daemon error: ' . $curl_error($curl));
+            return null;
+        } else {
+            log::add('genepi','debug','Daemon response: ' . $curl_response);
+            return $curl_response;
+        }
+    }
+
+
+    // check if genepii daemon responds
+    public static function check() {
+        $gateway = config::byKey('ip','genepi');
+        log::add('genepi','debug','Check genepi GW ' . $gateway);
+        $pouet = genepi::sendToDaemon('check');
+
+        return "YEP YEP YEP";
+    }
+
+
+    // check if genepii daemon responds
+    public static function getCapabilities() {
+        $gateway = config::byKey('ip','genepi');
+        log::add('genepi','debug','Check genepi GW ' . $gateway);
+        $pouet = genepi::sendToDaemon('capabilities');
+
+        return "YEP YEP YEP";
+    }
+
+
+    /*
+     * Fonction exécutée automatiquement toutes les minutes par Jeedom
+      public static function cron() {
+
+      }
+     */
+
+
+    /*
+     * Fonction exécutée automatiquement toutes les heures par Jeedom
+      public static function cronHourly() {
+
+      }
+     */
+
+    /*
+     * Fonction exécutée automatiquement tous les jours par Jeedom
+      public static function cronDayly() {
+
+      }
+     */
+
+
+
+    /*     * *********************Méthodes d'instance************************* */
+
+    public function preInsert() {
+        
+    }
+
+    public function postInsert() {
+        
+    }
+
+    public function preSave() {
+        
+    }
+
+    public function postSave() {
+        
+    }
+
+    public function preUpdate() {
+        
+    }
+
+    public function postUpdate() {
+        
+    }
+
+    public function preRemove() {
+        
+    }
+
+    public function postRemove() {
+        
+    }
+
+    /*
+     * Non obligatoire mais permet de modifier l'affichage du widget si vous en avez besoin
+      public function toHtml($_version = 'dashboard') {
+
+      }
+     */
+
+    /*     * **********************Getteur Setteur*************************** */
+}
+
+class genepiCmd extends cmd {
+    /*     * *************************Attributs****************************** */
+
+
+    /*     * ***********************Methode static*************************** */
+
+
+    /*     * *********************Methode d'instance************************* */
+
+    /*
+     * Non obligatoire permet de demander de ne pas supprimer les commandes même si elles ne sont pas dans la nouvelle configuration de l'équipement envoyé en JS
+      public function dontRemoveCmd() {
+      return true;
+      }
+     */
+
+    public function execute($_options = array()) {
+        
+    }
+
+    /*     * **********************Getteur Setteur*************************** */
+}
+
+?>
---
+++ plugin_info/configuration.php
@@ -0,0 +1,85 @@
+<?php
+/* This file is part of Jeedom.
+ *
+ * Jeedom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Jeedom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
+include_file('core', 'authentification', 'php');
+if (!isConnect()) {
+    include_file('desktop', '404', 'php');
+    die();
+}
+?>
+<form class="form-horizontal">
+    <fieldset>
+        <div class="form-group">
+            <label class="col-lg-4 control-label">{{Nom du GenePi}}</label>
+            <div class="col-lg-2">
+                <input class="configKey form-control" data-l1key="name" />
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-lg-4 control-label">{{Adresse IP}}</label>
+            <div class="col-lg-2">
+                <input class="configKey form-control" data-l1key="ip" value="80" />
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-lg-4 control-label">{{Global param 2}}</label>
+            <div class="col-lg-2">
+                <select class="configKey form-control" data-l1key="param3">
+                    <option value="value1">value1</option>
+                    <option value="value2">value2</option>
+                </select>
+            </div>
+            <div class="col-lg-3">
+                <a class="btn btn-success bt_check"><i class="fa fa-check-circle"></i>Test</a>
+            </div>
+        </div>
+  </fieldset>
+</form>
+
+<script>
+$('.bt_check').on('click',function(){
+  $.ajax({// fonction permettant de faire de l'ajax
+  type: "POST", // méthode de transmission des données au fichier php
+  url: "plugins/genepi/core/ajax/genepi.ajax.php", // url du fichier php
+  data: {
+    action: "check",
+  },
+  dataType: 'json',
+  global: false,
+  error: function (request, status, error) {
+    handleAjaxError(request, status, error);
+  },
+  success: function (data) { // si l'appel a bien fonctionné
+  if (data.state != 'ok') {
+    $('#div_alert').showAlert({message: data.result, level: 'danger'});
+    return;
+  } else {
+    $('.bt_check').text(data.result);
+//    window.location.href = 'index.php?v=d&p=plugin&id=genepi';
+  }
+}
+});
+});
+
+/*
+$('.eqLogicAction[data-action=add]').on('click', function () {
+    bootbox.prompt("{{Nom de l'équipement ?}}", function (result) {
+        if (result !== null) {
+*/
+</script>
+
